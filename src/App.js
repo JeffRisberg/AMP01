4c46dd54716d2e663e1aa6987a91c5299a548c72
JeffRisberg <jrisberg@aol.com>
2019-03-09 16:45:02 -0800

    end of lecture 13

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import {withAuthenticator} from 'aws-amplify-react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    );
-  }
+    state = {
+        notes: [
+            {id: 1, note: "Hello World"}
+        ]
+    };
+
+    render() {
+        const {notes} = this.state;
+
+        return (
+            <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+                <h1 className="code f2-1">
+                    Amplify Notetaker
+                </h1>
+                <form className="mb3">
+                    <input className="pa2 f4"
+                           placeholder="Write your note"/>
+                    <button className="pa2 f4" type="submit">
+                        Add Note
+                    </button>
+                </form>
+
+                {/* notes list */}
+                <div>
+                    {notes.map(item => (
+                        <div key={item.id} className="flex items-center">
+                            <li className="list pa1 f3">{item.note}</li>
+                            <button className="bg-transparent bn f4">
+                                <span>&times</span>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withAuthenticator(App);
